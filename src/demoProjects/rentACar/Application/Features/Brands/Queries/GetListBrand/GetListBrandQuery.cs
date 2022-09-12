6af48002f5fdddad0091cd03df523aa9cbed074d
Kayhan <kayhankaya34@gmail.com>
2022-09-12 16:22:51 +0300

    Day 4- Complex Modelling- Dynamic Search

--- src/demoProjects/rentACar/Application/Features/Brands/Queries/GetListBrand/GetListBrandQuery.cs
+++ src/demoProjects/rentACar/Application/Features/Brands/Queries/GetListBrand/GetListBrandQuery.cs
@@ -14,28 +14,27 @@ using System.Threading.Tasks;
 
 namespace Application.Features.Brands.Queries.GetListBrand
 {
-    public class GetListBrandQuery:IRequest<BrandListModel>
+    public class GetListBrandQuery : IRequest<BrandListModel>
     {
         public PageRequest PageRequest { get; set; }
-        public class GetListBrandQueryHandler:IRequestHandler<GetListBrandQuery,BrandListModel>
+        public class GetListBrandQueryHandler : IRequestHandler<GetListBrandQuery, BrandListModel>
         {
             private readonly IBrandRepository _brandRepository;
+            private readonly IMapper _mapper;
 
-            public GetListBrandQueryHandler(IBrandRepository brandRepository,IMapper mapper)
+            public GetListBrandQueryHandler(IBrandRepository brandRepository, IMapper mapper)
             {
                 _brandRepository = brandRepository;
                 _mapper = mapper;
             }
 
-            private readonly IMapper _mapper;
-
             public async Task<BrandListModel> Handle(GetListBrandQuery request, CancellationToken cancellationToken)
             {
-                IPaginate<Brand> brands=await _brandRepository.GetListAsync(index: request.PageRequest.Page, size: request.PageRequest.PageSize);
+                IPaginate<Brand> brands = await _brandRepository.GetListAsync(index: request.PageRequest.Page, size: request.PageRequest.PageSize);
                 BrandListModel mappedBrandListModel = _mapper.Map<BrandListModel>(brands);
                 return mappedBrandListModel;
             }
         }
-        
+
     }
 }
